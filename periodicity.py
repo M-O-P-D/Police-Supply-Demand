c362ec88afd1de6fd6593f27c866e090a1aae26d
Andrew Smith <andrew@friarswood.net>
2021-01-27 16:53:13 +0000

    add processing code for periodic data

--- periodicity.py
+++ periodicity.py
@@ -23,7 +23,13 @@ def fix_code(c):
   # adjust or return input
   return code_adjustments.get(c, c)
 
+def dow_map(dow):
+  weekdays = ["M","T","W","Th","F","S","Su"]
+  return weekdays[dow]
+
 crimes = pd.read_csv("./data/Playing_Periodicity.csv").drop(["MonthCreated","WeekCreated", "DayCreated"], axis=1)
+
+print(crimes.head())
 
 # expand out counts
 total = crimes.TotalCreated.sum()
@@ -38,33 +44,52 @@ crimes.drop(["xcor_code.1", "xcor_lkhocsubcodename"], axis=1, inplace=True)
 # adjust day number so that 0 is monday, 6 is sunday
 crimes.DayNumber = crimes.DayNumber.apply(lambda d: d-1)
 crimes.TimeWindow = crimes.TimeWindow.apply(intraday_enum)
-crimes["period"] = crimes.DayNumber * 3 + crimes.TimeWindow
 
 #crimes.set_index(["xcor_code", "xcor_lkhoccodename"], inplace=True)
 #crimes.set_index(["YearCreated", "MonthNumber", "DayNumber", "TimeWindow"], inplace=True)
 
-print(crimes.head())
+# YearCreated  MonthNumber  DayNumber  TimeWindow xcor_code      xcor_lkhoccodename
+crimes = crimes.groupby(["YearCreated", "MonthNumber", "DayNumber", "TimeWindow", "xcor_code", "xcor_lkhoccodename"], as_index=False).size() \
+  .rename({"YearCreated": "year", "MonthNumber": "month", "DayNumber": "dow", "TimeWindow": "time", "size": "count"}, axis=1)
+# crimes = crimes.groupby(["DayNumber", "TimeWindow", "xcor_code", "xcor_lkhoccodename"], as_index=False).size() \
+#   .rename({"DayNumber": "dow", "TimeWindow": "time", "size": "count"}, axis=1)
 
-crimes["count"] = 1
+assert crimes["count"].sum() == total
+
+print(crimes.head())
 
 # print(crimes.xcor_code.unique())
 
-weekly = crimes[crimes.YearCreated==2020].drop(["YearCreated", "MonthNumber", "DayNumber", "TimeWindow"], axis=1) \
-               .groupby(["xcor_code", "xcor_lkhoccodename", "period"]).sum() \
+# #[crimes.YearCreated==2020]
+weekly = crimes.drop(["year", "month", "time"], axis=1) \
+               .groupby(["xcor_code", "xcor_lkhoccodename", "dow"]).sum() \
                .reset_index().set_index(["xcor_code", "xcor_lkhoccodename"])
-
 print(weekly.head())
 
-#assert weekly["count"].sum() == total
+assert weekly["count"].sum() == total
 
-weekly.to_csv("./data/weekly.csv")
+# weekly.to_csv("./data/weekly.csv")
 
 # w = weekly.loc[("04-1", "Manslaughter")]
 # print(w)
 # plt.plot(w.period.values/3, w["count"].values, "o")
 
-w = weekly.loc[("105A", "Assault Without Injury")]
-print(w)
-plt.plot(w.period.values/3, w["count"].values, "o")
+weekly.dow = weekly.dow.apply(dow_map)
 
-plt.show()
+totals = weekly.reset_index().groupby(["xcor_code", "xcor_lkhoccodename"]).sum()
+totals = totals[totals["count"] > 49]
+
+ngraphs = len(totals)
+print(len(totals))
+
+for i in totals.index:
+  print(i[1])
+  w = weekly.loc[i]
+  plt.cla()
+  plt.bar(w.dow, w["count"].values)
+  plt.ylabel("Count (2y period)")
+  plt.title("%s (%s)" % (i[1], i[0]))
+  plt.savefig("doc/weekly-%s.png" % i[0].replace("/", "-"))
+
+
+#plt.show()
