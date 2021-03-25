df39d17b402a4c13324413f38cec65278915c9e3
virgesmith <a.p.smith@leeds.ac.uk>
2021-03-25 19:09:48 +0000

    use java sge module

--- batch.sh
+++ batch.sh
@@ -9,7 +9,7 @@
 #$ -e log
 #$ -pe ib 20
 
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.222.b10-0.el7_6.x86_64/jre
+module load java
 
 ~/NetLogo\ 6.2.0/netlogo-headless.sh --model event-response-with-shifts.nlogo --setup-file experiments/test-batch.xml
 
