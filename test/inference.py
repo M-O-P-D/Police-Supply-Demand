133b01c1beca1ca59038ef678ca963adea462118
virgesmith <andrew@friarswood.net>
2021-04-27 09:20:15 +0100

    try using daily/weekly rather than combined

--- test/inference.py
+++ test/inference.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import pandas as pd
 from scipy import stats
 
 import matplotlib.pyplot as plt
@@ -23,22 +24,39 @@ def posterior(w, obs):
 
   #print(c.head())
   count = obs.sum()
-
   alpha = np.full(len(obs), YEARS_OF_DATA * w / 3.0)
 
-  print(alpha)
-  print(obs)
+  #print(alpha)
+  #print(obs)
 
   expectation = count * stats.dirichlet.mean(alpha + obs)
-
   stddev = count * np.sqrt(stats.dirichlet.var(alpha + obs))
 
   return expectation, stddev
+
+def posterior_orth(w, obs_w, obs_d):
+
+  count = obs_w.sum() # == obs_d.sum()
+
+  w_exp, w_sd = posterior(w, obs_w)
+  d_exp, d_sd = posterior(w, obs_d)
+
+  w_var = (w_sd / count) ** 2
+  d_var = (d_sd / count) ** 2
+
+  var = np.tile(d_var, 7) + np.repeat(w_var, 3)
+
+  expectation = np.reshape(np.outer(w_exp / w_exp.sum(), d_exp / d_exp.sum()), 21) * obs_w.sum()
+  # add variances?
+  stddev = count * np.sqrt(var)
+  return expectation, stddev
+
 
 def plot(ctype, weight, ax, obs, expectation, stddev):
   ax.bar(x, obs, width=0.5, alpha=1.0, label="Observed events")
   ax.errorbar(x, expectation, fmt='o', yerr=stddev, color="r", label="Posterior distribution")
   ax.fill_between(x, expectation-stddev, expectation+stddev, alpha=0.2, color="orange")
+  ax.set_xticklabels(x, rotation=90, ha='right')
   ax.set_title("%s Prior weight=%.1f/y Samples=%.1f/y" % (ctype, weight, np.sum(obs)/YEARS_OF_DATA))
 
 # no of categories
@@ -46,8 +64,12 @@ YEARS_OF_DATA = 2
 
 data = decrypt_csv("./data/weekly-weights.csv.enc", index_col=["xcor_code", "period"])
 
+data_w = pd.read_csv("./weekly_adjusted.csv", index_col=["xcor_code", "DayNumber"])
+data_d = pd.read_csv("./daily_adjusted.csv", index_col=["xcor_code", "TimeWindow"])
+
+
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15,5))
-plt.tight_layout()
+#plt.tight_layout()
 
 #types = ["1", "10A", "17B", "22B", "30C", "8R", "105A"]
 ctype = "17B"
@@ -55,15 +77,31 @@ ctype = "17B"
 obs = data.loc[ctype, "count"]
 
 w = 1.0 # per year
+
+# or... how do we compute variance
+obs_w = data_w.loc[ctype, "count"]
+obs_d = data_d.loc[ctype, "count"]
+
 expectation, stddev = posterior(w, obs)
+#expectation, stddev = posterior_orth(w, obs_w, obs_d)
+
+# print(exp.sum())
+# print(expectation.sum())
+# print(std.sum())
+# print(stddev.sum())
+# print(std)
+# print(stddev)
+# stop
 plot(ctype, w, ax1, obs, expectation, stddev)
 
 w = 4.0 # per year
 expectation, stddev = posterior(w, obs)
+#expectation, stddev = posterior_orth(w, obs_w, obs_d)
 plot(ctype, w, ax2, obs, expectation, stddev)
 
 w = 16.0 # per year
 expectation, stddev = posterior(w, obs)
+#expectation, stddev = posterior_orth(w, obs_w, obs_d)
 plot(ctype, w, ax3, obs, expectation, stddev)
 
 # ax1.bar(x, obs, width=0.5, alpha=1.0, label="Observed events")
